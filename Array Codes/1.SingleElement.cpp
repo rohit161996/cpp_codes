8b3e42ea01a419af2e86c649d4f4729fab0dd3f5
rohit161996 <rohitramchandanistm1996@gmail.com>
2024-09-03 19:53:09 +0530

    Single Element, Sort0s1s and Two Sum Codes added

---
+++ Array Codes/1.SingleElement.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findSingleElement(int arr[], int size)
+{
+    int ans = 0;
+
+    // To find the final answer we need to XOR all the elements
+    for(int i=0; i<size; i++){
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+int main(){
+    int arr[] = {2,5,2,1,5,4,3,1,3};
+    int size = 9;
+
+    int ans;
+    ans = findSingleElement(arr, size);
+    cout << "The single element is " << ans << endl;
+    return 0;
+}
---
+++ Array Codes/3.TwoSum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> twoSum(int arr[], int size, int target){
+    pair<int, int> ans = make_pair(-1,-1);
+    for(int i=0; i<size; i++){
+        for(int j=i+1; j<size; j++){
+            if(arr[i]+arr[j]==target){
+                ans.first = arr[i];
+                ans.second = arr[j];
+                return ans;
+            }
+        }
+    }
+    // There is no pair equal to target
+    return ans;
+}
+
+int main(){
+    int arr[] = {10, 5, 20, 15, 30};
+    int size = 5;
+    int target = 35;
+
+    pair<int,int> ans = twoSum(arr, size, target);
+    cout << "The pair is "<< ans.first <<" and "<< ans.second;
+    return 0;
+}
